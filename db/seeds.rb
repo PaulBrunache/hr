ebc0ba402be65fcd715b3e0957724b7687a8afca
paulbrunache <paulbrunache@gmail.com>
2016-09-25 13:28:23 -0400

    modified seed data to convert it to a string

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-departments = {
-  Row Labels,FDN,Foundation,Executive,FINANCE,HR,IT,MEA,Grant,Kramer Senior Service Agy,MGC,ADMINISTRATIVE SERVICES
-  ,ADMISSIONS,CULN/NUTRITION SVCS,FACILITIES MANAGEMENT,FISCAL,HOUSEKEEPING,INSTITUTE
-  ,MARKET DEVELOPMENT,MEDICAL SERVICES,MEMORY CARE,None Specified,NURSING SERVICES,PURCHASING,REHAB SERVICES
-  ,SOCIAL SERVICES,THERAPEUTIC RECREATION,THRIFT SHOP,MHC,Administration,Culinary Services,HOUSEKEEPING
-  ,Maintenance,Marketing,Medical Office,NURSING SERVICES,Resident Services,MHH,Adult Day Care,HomeCare- Medicare
-  ,Kramer Senior Service Agy,MorseLifeHomeCare-Private,PACE
-}
+department_list = ["Row Labels","FDN","Foundation","Executive","FINANCE","HR","IT","MEA","Grant","Kramer Senior Service Agy","MGC","ADMINISTRATIVE SERVICES
+","ADMISSIONS","CULN/NUTRITION SVCS","FACILITIES MANAGEMENT","FISCAL","HOUSEKEEPING","INSTITUTE","MARKET DEVELOPMENT
+","MEDICAL SERVICES","MEMORY CARE","None Specified","NURSING SERVICES","PURCHASING","REHAB SERVICES","SOCIAL SERVICES
+","THERAPEUTIC RECREATION","THRIFT SHOP","MHC","Administration","Culinary Services","HOUSEKEEPING","Maintenance
+","Marketing","Medical Office","NURSING SERVICES","Resident Services","MHH","Adult Day Care","HomeCare- Medicare
+","Kramer Senior Service Agy","MorseLifeHomeCare-Private","PACE"]
+department_list.each do |department|
+  Department.create!( name: department)
+end
