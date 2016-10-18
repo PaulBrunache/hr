30468337686f045280864d1c9c30b363ab29143b
paulbrunache <paulbrunache@gmail.com>
2016-10-17 20:44:18 -0400

    removed test folder

--- test/controllers/employee/dashboard_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Employee::DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get employee_dashboard_index_url
-    assert_response :success
-  end
-
-end
