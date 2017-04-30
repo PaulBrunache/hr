1ae8a8d196a258ed2ef085c2c970d8760d469035
paulbrunache <paulbrunache@gmail.com>
2017-04-30 06:33:59 -0400

    + fixed backdoor access to registrations * fixed broken reset password for hr

--- app/controllers/human_resources/registrations_controller.rb
+++ app/controllers/human_resources/registrations_controller.rb
@@ -1,14 +1,19 @@
 class HumanResources::RegistrationsController < Devise::RegistrationsController
-  protected
-    # def after_sign_up_path_for(resource)
-    #   signed_in_root_path(resource)
-    # end
-    #
-    def after_update_path_for(resource)
-      edit_human_resource_registration_path
-    end
-    def account_update_params
-      params.require(:human_resource).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-    end
+
+  def new
+    flash[:info] = 'Registrations are not open.'
+    redirect_to root_path
+  end
+  def create
+    flash[:info] = 'Registrations are not open.'
+    redirect_to root_path
+  end
+
+  def after_update_path_for(resource)
+    edit_human_resource_registration_path
+  end
+  def account_update_params
+    params.require(:human_resource).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
 
 end
